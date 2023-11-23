7628f2b8f4239c55ee2be85f965a9b1d77e51253
Kate <lazar.k@wp.pl>
2023-11-23 18:02:02 +0100

    first commit

---
+++ src/main/java/com/example/apolloapp/ApolloAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apolloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apolloapp/ApolloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apolloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
