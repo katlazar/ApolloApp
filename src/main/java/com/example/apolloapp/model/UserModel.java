d8e5c9fcaafb39bf1fc673a57a09180a8bf18c9b
Kate <lazar.k@wp.pl>
2023-11-23 18:59:19 +0100

    task1: add folders and UserModule

---
+++ src/main/java/com/example/apolloapp/model/UserModel.java
@@ -0,0 +1,32 @@
+package com.example.apolloapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="type")
+    private String type;
+
+    @Column(name="login")
+    private String login;
+
+    @Column(name="password")
+    private String password;
+
+}
