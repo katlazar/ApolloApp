8e2b058594bf9a0338998ca094ef45f36824b83d
Kate <lazar.k@wp.pl>
2023-11-25 08:49:50 +0100

    add EnrollmentModule

---
+++ src/main/java/com/example/apolloapp/model/EnrollmentModel.java
@@ -0,0 +1,36 @@
+package com.example.apolloapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name="enrollment")
+public class EnrollmentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserModel user;
+
+//    @OneToOne
+//    @JoinColumn(name = "course_id")
+//    private CourseModel course;
+
+    @Column(name="creation_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate creationDate;
+
+    @Column(name="payment_status")
+    private Boolean paymentStatus;
+
+    @Column(name="course_price")
+    private Double coursePrice;
+}
