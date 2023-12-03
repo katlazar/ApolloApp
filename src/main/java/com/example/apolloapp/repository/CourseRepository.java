44bdafa9cecfbf8032b0e4beabee152ad3218c5a
EwaZwierzynska <e.m.zwierzynska@gmail.com>
2023-12-03 13:14:39 +0100

    add CourseRepository and ModuleRepository interfaces

---
+++ src/main/java/com/example/apolloapp/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.apolloapp.repository;
+
+import com.example.apolloapp.model.CourseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<CourseModel, Long> {
+}
