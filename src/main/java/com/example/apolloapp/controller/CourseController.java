a64a4d15d50af4b6556475fcb7390e462e7fbe13
EwaZwierzynska <e.m.zwierzynska@gmail.com>
2023-12-03 16:38:26 +0100

    add courseList HTML view and empty UserController class
    CourseRepository and ModuleRepository interfaces

--- src/main/java/com/example/apolloapp/controller/CourseController.java
+++ src/main/java/com/example/apolloapp/controller/CourseController.java
@@ -4,14 +4,18 @@ import com.example.apolloapp.model.CourseModel;
 import com.example.apolloapp.repository.CourseRepository;
 import com.example.apolloapp.service.CourseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+//@RequestMapping(value = "/cars", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class CourseController {
 
     private final CourseService courseService;
@@ -21,8 +25,8 @@ public class CourseController {
     public String getCourseList(Model model){
         List<CourseModel> list = courseService.getCourseList();
         model.addAttribute("courseModel", list);
-        return "courses/courses";
-        // nie pamiętam dlaczego mamy podwójnie courses? ---> dlatego że z button courses wchodzimy w właściwą listę kursów / widok kursó?
+        return "courseList";
+        //ze strony głównej nie przełącza na Courses
     }
 
 
