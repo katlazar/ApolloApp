e8e4ea3afde7246b94853217b9cf6349cad48aad
Kate <lazar.k@wp.pl>
2023-12-09 12:33:33 +0100

    Login and Logout validation

--- src/main/java/com/example/apolloapp/config/SecurityConfig.java
+++ src/main/java/com/example/apolloapp/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .formLogin(
 
                         form -> form
-                                .loginPage("/")
+                                .loginPage("/login")
                                 .loginProcessingUrl("/login")
                                 .defaultSuccessUrl("/")
                                 .permitAll()
