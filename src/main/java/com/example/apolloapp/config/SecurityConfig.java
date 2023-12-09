fad72fc3cd79c27d8515babb37253b960d37f910
EwaZwierzynska <e.m.zwierzynska@gmail.com>
2023-12-09 15:08:40 +0100

    add fix to SecurityConfig and data.sql AGAIN

--- src/main/java/com/example/apolloapp/config/SecurityConfig.java
+++ src/main/java/com/example/apolloapp/config/SecurityConfig.java
@@ -30,11 +30,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->  // potem będzie rozbudowane o uprawnienia użytkowników
                         authorize
                                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-//                                .anyRequest().permitAll() // aplikacja widoczna dla wszystkich i funkcjonalna ->przełącza na inne widoki
-                                .requestMatchers("/").permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/adminPanel")).hasAnyRole("ADMIN")
-                                .requestMatchers(new AntPathRequestMatcher("/lecturerPanel")).hasAnyRole("TEACHER")
-                                .requestMatchers(new AntPathRequestMatcher("/studentPanel")).hasAnyRole("STUDENT")
+                                .anyRequest().permitAll() // aplikacja widoczna dla wszystkich i funkcjonalna ->przełącza na inne widoki
+//                                .requestMatchers("/").permitAll()
+//                                .requestMatchers(new AntPathRequestMatcher("/adminPanel")).hasAnyRole("ADMIN")
+//                                .requestMatchers(new AntPathRequestMatcher("/lecturerPanel")).hasAnyRole("TEACHER")
+//                                .requestMatchers(new AntPathRequestMatcher("/studentPanel")).hasAnyRole("STUDENT")
 
 //                                .anyRequest().authenticated() // włączenie widoczności dla użytkowników tylko zalogowanych
                 )
